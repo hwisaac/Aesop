bf5336b5406120c0bd06c394d7c397440aefb134
Hyerim Han <hyerim_han@naver.com>
2022-10-11 17:27:14 +0900

    feat: perfume area swiper render

---
+++ HyeRim/js/main.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.perfume.swiper', {
+  loop: false,
+  slidesPerView: 4,
+  spaceBetween: 50,
+  speed: 700,
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    640: {
+      slidesPerView: 2
+    },
+    1080: {
+      slidesPerView: 3
+    },
+    2000: {
+      slidesPerView: 4
+    }
+  },
+  scrollbar: {
+    el: '.perfume .swiper-scrollbar',
+    dragSize: 200,
+  },
+  navigation: {
+    nextEl: '.perfume .swiper-button-next',
+    prevEl: '.perfume .swiper-button-prev',
+  },
+});
