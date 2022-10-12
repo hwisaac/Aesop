43035235d052d26c35fb2a95ad2ad90e08ade19e
hae9 <gorud0929@naver.com>
2022-10-12 15:01:53 +0900

    20221012-swiper완성

--- Haekyung/main.js
+++ Haekyung/main.js
@@ -6,11 +6,45 @@ new Swiper('.sec2 .swiper', {
   spaceBetween: 15,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.sec2 .inner .swiper-button-next',
+    prevEl: '.sec2 .inner .swiper-button-prev',
   },
 
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: '.sec2 .swiper-scrollbar',
+  },
+});
+
+new Swiper('.sec5 .swiper', {
+  observe: true,
+  observeParents: true,
+  loop: false,
+  slidesPerView: 3.2,
+  spaceBetween: 42,
+
+  navigation: {
+    nextEl: '.sec5 .swiper-button-next',
+    prevEl: '.sec5 .swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.sec5 .inner .swiper .swiper-scrollbar',
+  },
+});
+
+new Swiper('.sec6 .inner .swiper', {
+  observe: true,
+  observeParents: true,
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+
+  navigation: {
+    nextEl: '.sec6 .swiper-button-next',
+    prevEl: '.sec6 .swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.sec6 .inner .swiper .swiper-scrollbar',
   },
 });
