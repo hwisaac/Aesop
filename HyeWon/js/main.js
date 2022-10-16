6697e9cc057a497899b4e1b282f11811ee7ad106
Wonny-ing <h.wonny11@gmail.com>
2022-10-16 12:03:00 +0900

    Invised gnb modal window

--- HyeWon/js/main.js
+++ HyeWon/js/main.js
@@ -1,3 +1,4 @@
+// Perfume section Swiper
 // new Swiper('선택자', 옵션);
 new Swiper('#perfume-section .swiper', {
   // autoplay: true,
@@ -14,6 +15,7 @@ new Swiper('#perfume-section .swiper', {
   },
 });
 
+// Selection section Swiper
 new Swiper('#selection-section .swiper', {
   slidesPerView: 4,
   spaceBetween: 20,
@@ -28,6 +30,7 @@ new Swiper('#selection-section .swiper', {
   },
 });
 
+// Store section Swiper
 const paginationEl = document.querySelector('#store-section .swiper-pagination');
 new Swiper('#store-section .swiper', {
   // loop: true,
@@ -80,11 +83,9 @@ new Swiper('#store-section .swiper', {
   },
 });
 
-//  Header
-const headerEl = document.querySelector('.header');
-
-// Press the Header button to open a modal window
+//  Header (최상단 띠배너) 클릭시 Modal창 오픈
 const body = document.querySelector('body');
+const headerEl = document.querySelector('.header');
 const headerBtn = document.querySelector('.header .header-btn');
 const modal = document.querySelector('.header .modal');
 
@@ -97,7 +98,7 @@ const openModal = () => {
 
 headerBtn.addEventListener('click', openModal);
 
-// Close the modal window by pressing the Modal button
+// Modal button 및 Modal창 외부영역 클릭 시, modal창 닫힘
 const overlay = document.querySelector('.modal .modal__overlay');
 const modalBtn = document.querySelector('.modal .modal-btn');
 
@@ -111,10 +112,35 @@ const closeModal = () => {
 overlay.addEventListener('click', closeModal);
 modalBtn.addEventListener('click', closeModal);
 
-// Press the GNB to open a modal window
-const gnbItems = document.querySelectorAll('.gnb .gnb-item');
+// GNB 각 카테고리 시, 해당 GNB modal창 오픈 및 Header(최상단 띠배너) 사라짐
+// Modal elements
+// const skinCareModalEl = document.querySelector('.gnb-item .skin-care-modal');
+// const bodyHandModalEl = document.querySelector('.gnb-item .body-hand-modal');
+// const hairModalEl = document.querySelector('.gnb-item .hair-modal');
+// const perfumeModalEl = document.querySelector('.gnb-item .perfume-modal');
+// const homeModalEl = document.querySelector('.gnb-item .home-modal');
+// const kitModalEl = document.querySelector('.gnb-item .kit-modal');
+// const giftModalEl = document.querySelector('.gnb-item .gift-modal');
+// const readingModalEl = document.querySelector('.gnb-item .reading-modal');
+// const storeModalEl = document.querySelector('.gnb-item .store-modal');
+// const searchModalEl = document.querySelector('.gnb-item .search-modal');
+
+// let gnbItemModals = [
+//   skinCareModalEl,
+//   bodyHandBtnEl,
+//   hairBtnEl,
+//   perfumeBtnEl,
+//   homeBtnEl,
+//   kitBtnEl,
+//   giftBtnEl,
+//   readingBtnEl,
+//   storeBtnEl,
+//   searchBtnEl,
+// ];
+
+const gnbItemEls = document.querySelectorAll('.gnb .gnb-item a');
 const gnb = document.querySelector('.gnb');
-const gnbBox = document.querySelector('.gnb .gnb-box');
+const gnbContainer = document.querySelector('.gnb .gnb-container');
 const gnbItem = gnb.querySelector('.gnb-item');
 const closeBtn = gnb.querySelector('.gnb-item.close');
 
@@ -122,34 +148,46 @@ const gnbModals = document.querySelectorAll('.gnb-item .gnb-modal');
 const gnbModal = document.querySelector('.gnb-item .gnb-modal');
 const header = document.querySelector('.header');
 
-const openGnbModal = () => {
-  gnbModal.classList.add('open');
-  closeBtn.classList.remove('hidden');
-  if (gnbModal.classList.contains('open')) {
-    body.style.overflow = 'hidden';
+// const openGnbModal = () => {
+//   gnbModal.classList.add('open');
+//   closeBtn.classList.remove('hidden');
+//   if (gnbModal.classList.contains('open')) {
+//     body.style.overflow = 'hidden';
+//     headerEl.classList.add('hidden');
+//     gnbBox.classList.add('open');
+//   }
+// };
+
+// gnbItem.addEventListener('click', openGnbModal);
+
+//forEach
+gnbItemEls.forEach((gnbItemEl, gnbItemIndex) => {
+  gnbItemEl.addEventListener('click', () => {
+    const gnbItemNextSib = gnbItemEl.nextElementSibling;
+    gnbItemNextSib.classList.toggle('open');
+    closeBtn.classList.remove('hidden');
     headerEl.classList.add('hidden');
-    gnbBox.classList.add('open');
-  }
-};
+    body.style.overflow = 'hidden';
+  });
+});
 
-gnbItem.addEventListener('click', openGnbModal);
+// const gnbItemModals = [];
+// gnbItemEls.forEach((gnbItemEl)=>{
+//   gnbItemModals.push(gnbItemEl);
+// })
 
-// gnbItems.forEach((gnbItem, gindex) => {
-//   gnbModals.forEach((gnbModal, mindex) => {
-//     if (gindex === mindex) {
-//       gnbItem.addEventListener('click', openGnbModal);
-//     }
-//   });
-// });
+// gnbContainer.addEventListener('click', openGnbModal);
 
-const closeGnbModal = () => {
-  closeBtn.classList.add('hidden');
-  if (closeBtn.classList.contains('hidden')) {
-    gnbModal.classList.remove('open');
-    body.style.overflow = 'scroll';
-    headerEl.classList.remove('hidden');
-  }
-};
-closeBtn.addEventListener('click', closeGnbModal);
+// closeBtn.addEventListener('click', closeGnbModal);
+
+// const closeGnbModal = () => {
+//   closeBtn.classList.add('hidden');
+//   if (closeBtn.classList.contains('hidden')) {
+//     gnbModal.classList.remove('open');
+//     body.style.overflow = 'scroll';
+//     headerEl.classList.remove('hidden');
+//   }
+// };
+// closeBtn.addEventListener('click', closeGnbModal);
 
 // Press the GNB to close a modal window
