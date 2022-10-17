a7fdbec620fd940ee486d2cec9d94a08de2c69b5
Wonny-ing <h.wonny11@gmail.com>
2022-10-18 03:33:25 +0900

    Created the Gnb Modal windows event Applied the scroll event

--- HyeWon/js/main.js
+++ HyeWon/js/main.js
@@ -113,81 +113,224 @@ overlay.addEventListener('click', closeModal);
 modalBtn.addEventListener('click', closeModal);
 
 // GNB 각 카테고리 시, 해당 GNB modal창 오픈 및 Header(최상단 띠배너) 사라짐
-// Modal elements
-// const skinCareModalEl = document.querySelector('.gnb-item .skin-care-modal');
-// const bodyHandModalEl = document.querySelector('.gnb-item .body-hand-modal');
-// const hairModalEl = document.querySelector('.gnb-item .hair-modal');
-// const perfumeModalEl = document.querySelector('.gnb-item .perfume-modal');
-// const homeModalEl = document.querySelector('.gnb-item .home-modal');
-// const kitModalEl = document.querySelector('.gnb-item .kit-modal');
-// const giftModalEl = document.querySelector('.gnb-item .gift-modal');
-// const readingModalEl = document.querySelector('.gnb-item .reading-modal');
-// const storeModalEl = document.querySelector('.gnb-item .store-modal');
-// const searchModalEl = document.querySelector('.gnb-item .search-modal');
+// Gnb Modal elements
+const skinCareModalEl = document.querySelector('.gnb-item .skin-care-modal');
+const bodyHandModalEl = document.querySelector('.gnb-item .body-hand-modal');
+const hairModalEl = document.querySelector('.gnb-item .hair-modal');
+const perfumeModalEl = document.querySelector('.gnb-item .perfume-modal');
+const homeModalEl = document.querySelector('.gnb-item .home-modal');
+const kitModalEl = document.querySelector('.gnb-item .kit-modal');
+const giftModalEl = document.querySelector('.gnb-item .gift-modal');
+const readingModalEl = document.querySelector('.gnb-item .reading-modal');
+const storeModalEl = document.querySelector('.gnb-item .store-modal');
+const searchModalEl = document.querySelector('.gnb-item .search-modal');
 
-// let gnbItemModals = [
-//   skinCareModalEl,
-//   bodyHandBtnEl,
-//   hairBtnEl,
-//   perfumeBtnEl,
-//   homeBtnEl,
-//   kitBtnEl,
-//   giftBtnEl,
-//   readingBtnEl,
-//   storeBtnEl,
-//   searchBtnEl,
-// ];
+// Gnb elements
+const skinCareEl = document.querySelector('.skin-care');
+const bodyHandEl = document.querySelector('.gnb-item.body-hand');
+const hairEl = document.querySelector('.gnb-item.hair');
+const perfumeEl = document.querySelector('.gnb-item.perfume');
+const homeEl = document.querySelector('.gnb-item.home');
+const kitEl = document.querySelector('.gnb-item.kit');
+const giftEl = document.querySelector('.gnb-item.gift');
+const readingEl = document.querySelector('.gnb-item.reading');
+const storeEl = document.querySelector('.gnb-item.store');
+const searchEl = document.querySelector('.gnb-item.search');
+const closeBtn = document.querySelector('.gnb-item.close');
 
-const gnbItemEls = document.querySelectorAll('.gnb .gnb-item a');
 const gnb = document.querySelector('.gnb');
+const gnbItemEls = document.querySelectorAll('.gnb .gnb-item');
 const gnbContainer = document.querySelector('.gnb .gnb-container');
-const gnbItem = gnb.querySelector('.gnb-item');
-const closeBtn = gnb.querySelector('.gnb-item.close');
-
+const gnbBox = document.querySelector('.gnb .gnb-box');
 const gnbModals = document.querySelectorAll('.gnb-item .gnb-modal');
-const gnbModal = document.querySelector('.gnb-item .gnb-modal');
-const header = document.querySelector('.header');
 
-// const openGnbModal = () => {
-//   gnbModal.classList.add('open');
-//   closeBtn.classList.remove('hidden');
-//   if (gnbModal.classList.contains('open')) {
-//     body.style.overflow = 'hidden';
-//     headerEl.classList.add('hidden');
-//     gnbBox.classList.add('open');
-//   }
-// };
+// 내코드
 
-// gnbItem.addEventListener('click', openGnbModal);
-
-//forEach
-gnbItemEls.forEach((gnbItemEl, gnbItemIndex) => {
-  gnbItemEl.addEventListener('click', () => {
-    const gnbItemNextSib = gnbItemEl.nextElementSibling;
-    gnbItemNextSib.classList.toggle('open');
-    closeBtn.classList.remove('hidden');
-    headerEl.classList.add('hidden');
-    body.style.overflow = 'hidden';
+const openSkinCare = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
   });
-});
+  skinCareModalEl.classList.add('open');
+  if (skinCareModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  } else {
+    gnbBox.style.position = 'fixed';
+  }
+};
+skinCareEl.addEventListener('click', openSkinCare);
 
-// const gnbItemModals = [];
-// gnbItemEls.forEach((gnbItemEl)=>{
-//   gnbItemModals.push(gnbItemEl);
-// })
+const openBodyHand = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  bodyHandModalEl.classList.add('open');
+  if (bodyHandModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+};
+bodyHandEl.addEventListener('click', openBodyHand);
 
-// gnbContainer.addEventListener('click', openGnbModal);
+const openHair = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  hairModalEl.classList.add('open');
+  if (hairModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+};
+hairEl.addEventListener('click', openHair);
 
-// closeBtn.addEventListener('click', closeGnbModal);
+const openPerfume = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  perfumeModalEl.classList.add('open');
+  if (perfumeModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+  console.log('click');
+};
+perfumeEl.addEventListener('click', openPerfume);
 
-// const closeGnbModal = () => {
-//   closeBtn.classList.add('hidden');
-//   if (closeBtn.classList.contains('hidden')) {
-//     gnbModal.classList.remove('open');
-//     body.style.overflow = 'scroll';
-//     headerEl.classList.remove('hidden');
-//   }
-// };
-// closeBtn.addEventListener('click', closeGnbModal);
+const openHome = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  homeModalEl.classList.add('open');
+  if (homeModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+  console.log('click');
+};
+homeEl.addEventListener('click', openHome);
 
-// Press the GNB to close a modal window
+const openKit = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  kitModalEl.classList.add('open');
+  if (kitModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+  console.log('click');
+};
+kitEl.addEventListener('click', openKit);
+
+const openGift = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  giftModalEl.classList.add('open');
+  if (giftModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+  console.log('click');
+};
+giftEl.addEventListener('click', openGift);
+
+const openReading = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  readingModalEl.classList.add('open');
+  if (readingModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+  console.log('click');
+};
+readingEl.addEventListener('click', openReading);
+
+const openStore = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  storeModalEl.classList.add('open');
+  if (storeModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+  console.log('click');
+};
+storeEl.addEventListener('click', openStore);
+
+const openSearch = () => {
+  gnbModals.forEach((gnbModal) => {
+    gnbModal.classList.remove('open');
+  });
+  searchModalEl.classList.add('open');
+  if (searchModalEl.classList.contains('open')) {
+    closeBtn.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    headerEl.classList.add('hidden');
+    gnbBox.classList.add('open');
+  }
+  console.log('click');
+};
+searchEl.addEventListener('click', openSearch);
+
+const closeGnbModal = () => {
+  closeBtn.classList.add('hidden');
+  if (closeBtn.classList.contains('hidden')) {
+    gnbModals.forEach((gnbModal) => {
+      gnbModal.classList.remove('open');
+    });
+    body.style.overflow = 'scroll';
+    headerEl.classList.remove('hidden');
+    gnbBox.classList.remove('open');
+  }
+};
+closeBtn.addEventListener('click', closeGnbModal);
+
+// Gnb 효과 속성값 제거 및 변경
+function removeGnbBoxEffect() {
+  gnbBox.classList.remove('header--white');
+  gnbBox.style.position = 'absolute';
+}
+
+//윈도우 마우스 wheel 감지해서 Gnb 작동시키기
+window.addEventListener(
+  'wheel',
+  _.throttle(function (event) {
+    let wheel = event.wheelDeltaY;
+    // console.log(event.screenY);
+    if (event.scrollY === 0) {
+      // 최상단에 위치하면 모든 효과 제거
+      removeGnbBoxEffect();
+    } else if (wheel > 0 && window.scrollY > 128) {
+      // Gnb가 보이지 않을때, Gnb를 흰색으로 변경 및 fixed 시킴
+      gnbBox.classList.add('gnb--white');
+      gnbBox.style.position = 'fixed';
+    } else if (wheel > 0 && window.scrollY < 60) {
+      // 원래 Gnb가 위치한 자리에 도달하면 Gnb를 속성값 제거 및 absolute 변경
+      gnbBox.classList.remove('gnb--white');
+      gnbBox.style.position = 'absolute';
+    }
+  }, 10)
+);
